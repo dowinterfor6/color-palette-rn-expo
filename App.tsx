834294879a990f6613e221cfbdc5af29d9419b4f
Andrew Chan <dowinterfor6@gmail.com>
2020-06-26 15:35:30 -0700

    Add custom animation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator, TransitionSpecs } from '@react-navigation/stack';
+import { createStackNavigator, TransitionSpecs, HeaderStyleInterpolators } from '@react-navigation/stack';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
@@ -33,16 +33,60 @@ const test = {
   }
 }
 
+const MyTransition = {
+  gestureDirection: 'horizontal',
+  transitionSpec: {
+    open: TransitionSpecs.TransitionIOSSpec,
+    close: TransitionSpecs.TransitionIOSSpec,
+  },
+  headerStyleInterpolator: HeaderStyleInterpolators.forFade,
+  cardStyleInterpolator: ({ current, next, layouts }) => {
+    return {
+      cardStyle: {
+        transform: [
+          {
+            translateX: current.progress.interpolate({
+              inputRange: [0, 1],
+              outputRange: [layouts.screen.width, 0],
+            }),
+          },
+          {
+            rotate: current.progress.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, 0],
+            }),
+          },
+          {
+            scale: next
+              ? next.progress.interpolate({
+                inputRange: [0, 1],
+                outputRange: [1, 0.9],
+              })
+              : 1,
+          },
+        ],
+      },
+      overlayStyle: {
+        opacity: current.progress.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, 0.5],
+        }),
+      },
+    };
+  },
+};
+
 const MainStackScreen = () => {
   return (
     <MainStack.Navigator
       screenOptions={{
         gestureEnabled: true,
-        gestureDirection: 'horizontal',
+        // gestureDirection: 'horizontal',
         headerTitleAlign: 'center',
         // cardShadowEnabled: false,
-        // cardOverlayEnabled: false,
+        cardOverlayEnabled: true,
         // animationEnabled: false,
+        ...MyTransition,
       }}
       headerMode="screen"
     >
